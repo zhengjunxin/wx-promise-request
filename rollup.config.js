666b658814310e9a686176d45370e40546ba65a1
郑俊鑫 <18810544291@163.com>
2017-04-27 17:29:02 +0800

    同步更改moduleName

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,15 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
+import fs from 'fs'
 
 const isProduction = process.env.NODE_ENV === 'production'
+const getPackageName = () => JSON.parse(fs.readFileSync('./package.json', 'utf8')).name
 
 export default {
     entry: 'src/index.js',
     format: 'umd',
-    moduleName: 'wx-request',
+    moduleName: getPackageName(),
     plugins: [
         resolve({ jsnext: true, main: true }),
         commonjs(),
