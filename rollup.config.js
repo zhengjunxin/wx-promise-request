7306b512e0a945daeda450d790351ea5b47c0d8d
郑俊鑫 <18810544291@163.com>
2017-05-07 15:54:46 +0800

    use package.json var

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,23 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
-import fs from 'fs'
-import eslint from 'rollup-plugin-eslint'
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import eslint from 'rollup-plugin-eslint';
 
-const isProduction = process.env.NODE_ENV === 'production'
-const getPackageName = () => JSON.parse(fs.readFileSync('./package.json', 'utf8')).name
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
-    entry: 'src/index.js',
-    format: 'umd',
-    moduleName: getPackageName(),
-    plugins: [
-        eslint({exclude: 'node_modules/**'}),
-        resolve({ jsnext: true, main: true }),
-        commonjs(),
-        babel({
-            exclude: 'node_modules/**',
-        }),
-        (isProduction && uglify())
-    ],
-    dest: 'dist/index.js',
-}
+  entry: 'src/index.js',
+  format: 'umd',
+  moduleName: process.env.npm_package_name,
+  plugins: [
+    eslint({ exclude: 'node_modules/**' }),
+    resolve({ jsnext: true, main: true }),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+        (isProduction && uglify()),
+  ],
+  dest: 'dist/index.js',
+};
